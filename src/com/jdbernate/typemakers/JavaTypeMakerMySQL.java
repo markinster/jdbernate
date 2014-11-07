7d0ff78d7588e408940648ada7c9b99666500bd1
markinster <markinster@gmail.com>
2014-11-07 15:51:13 -0200

    Added Map<?,?>

--- src/com/jdbernate/typemakers/JavaTypeMakerMySQL.java
+++ src/com/jdbernate/typemakers/JavaTypeMakerMySQL.java
@@ -1,33 +1,29 @@
-/*
-*      REMINDER: Refactor this class for use MAP <06-11-2014>
-*/
-
 
 package com.jdbernate.typemakers;
 
+import java.util.Map;
+
 public class JavaTypeMakerMySQL implements IJavaTypeMaker {
+
+	private Map<String, String> types;
+
 	private static final String INT = "int";
 	private static final String INTEGER = "integer";
-
 	private static final String CHAR = "char";
 	private static final String VARCHAR = "varchar";
-
 	private static final String TINYTEXT = "tinytext";
 	private static final String SMALLTEXT = "smalltext";
 	private static final String MEDIUMTEXT = "mediumtext";
 	private static final String BIGTEXT = "bigtext";
-
 	private static final String SERIAL = "serial";
 	private static final String CHARACTER = "character";
 	private static final String DECIMAL = "decimal";
 	private static final String DOUBLE = "double";
 	private static final String TIME = "time";
-
 	private static final String TINYINT = "tinyint";
 	private static final String SMALLINT = "smallint";
 	private static final String MEDIUMINT = "mediumint";
 	private static final String BIGINT = "bigint";
-
 	private static final String FLOAT = "float";
 	private static final String REAL = "real";
 	private static final String NUMERIC = "numeric";
@@ -48,56 +44,48 @@ public class JavaTypeMakerMySQL implements IJavaTypeMaker {
 		}
 	}
 
+	private void createMapTypes() {
+		types.put(CHARACTER, "String");
+		types.put(VARCHAR, "String");
+		types.put(CHAR, "String");
+		types.put(BIGTEXT, "String");
+		types.put(MEDIUMTEXT, "String");
+		types.put(SMALLTEXT, "String");
+		types.put(TINYTEXT, "String");
+		types.put(INT, "Integer");
+		types.put(INTEGER, "Integer");
+		types.put(SERIAL, "Integer");
+		types.put(BIGINT, "Integer");
+		types.put(MEDIUMINT, "Integer");
+		types.put(SMALLINT, "Integer");
+		types.put(TINYINT, "Integer");
+		types.put(DECIMAL, "BigDecimal");
+		types.put(DOUBLE, "BigDecimal");
+		types.put(FLOAT, "BigDecimal");
+		types.put(REAL, "BigDecimal");
+		types.put(NUMERIC, "BigDecimal");
+		types.put(YEAR, "Short");
+		types.put(TIME, "Calendar");
+		types.put(DATE, "Calendar");
+	}
+
 	private String returnType(String type) {
 		type = type.toLowerCase();
 
-		if (type.startsWith(CHARACTER)) {
-			return "String";
-		} else if (type.equals(VARCHAR)) {
-			return "String";
-		} else if (type.equals(INT)) {
-			return "Integer";
-		} else if (type.equals(INTEGER)) {
-			return "Integer";
-		} else if (type.equals(SERIAL)) {
-			return "Integer";
-		} else if (type.equals(DECIMAL)) {
-			return "BigDecimal";
-		} else if (type.equals(DOUBLE)) {
-			return "BigDecimal";
-		} else if (type.startsWith(TIME)) {
-			return "Calendar";
-		} else if (type.startsWith(CHAR)) {
-			return "String";
-		} else if (type.startsWith(BIGTEXT)) {
-			return "String";
-		} else if (type.startsWith(MEDIUMTEXT)) {
-			return "String";
-		} else if (type.startsWith(SMALLTEXT)) {
-			return "String";
-		} else if (type.startsWith(TINYTEXT)) {
-			return "String";
-		} else if (type.startsWith(BIGINT)) {
-			return "Integer";
-		} else if (type.startsWith(MEDIUMINT)) {
-			return "Integer";
-		} else if (type.startsWith(SMALLINT)) {
-			return "Integer";
-		} else if (type.startsWith(TINYINT)) {
-			return "Integer";
-		} else if (type.startsWith(FLOAT)) {
-			return "BigDecimal";
-		} else if (type.startsWith(REAL)) {
-			return "BigDecimal";
-		} else if (type.startsWith(NUMERIC)) {
-			return "BigDecimal";
-		} else if (type.startsWith(YEAR)) {
-			return "Short";
-		} else if (type.startsWith(DATE)) {
-			return "Calendar";
-		}
+		if (type == null)
+			createMapTypes();
 
-		return "String";
+		String ret;
+		try {
+			ret = types.get(type);
+		} catch (Exception e) {
+			ret = "String";
+		}
+		
+		if (ret == null || ret.isEmpty())
+			ret = "String";
+		
+		return ret;
 	}
 
 	private String returnType(String type, String size) {
