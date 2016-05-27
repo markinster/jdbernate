82c0fe6ca041a7a8623348414be243ff7d0b5076
markinster <markinster@gmail.com>
2016-05-27 15:26:06 -0300

    Adicionado Cabecalho par licenca GPL em todos os codigos fonte

--- src/com/jdbernate/JDbernate.java
+++ src/com/jdbernate/JDbernate.java
@@ -1,3 +1,37 @@
+/**************************************************************************************
+ *                                                                                    
+ *   This program is free software: you can redistribute it and/or modify              
+ *   it under the terms of the GNU General Public License as published by             
+ *   the Free Software Foundation, either version 3 of the License, or                
+ *   (at your option) any later version.                                              
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   See the license terms in <http://www.gnu.org/licenses/>.
+ * 
+ ************************************************************************************ 
+ * 
+ *   Este programa é um software livre; você pode redistribuí-lo e/ou 
+ *   modificá-lo dentro dos termos da Licença Pública Geral GNU como 
+ *   publicada pela Fundação do Software Livre (FSF); na versão 3 da 
+ *   Licença, ou (na sua opinião) qualquer versão.
+ *
+ *   Este programa é distribuído na esperança de que possa ser útil, 
+ *   mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO
+ *   a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a
+ *   Licença Pública Geral GNU para maiores detalhes.
+ *
+ *   veja os termos da licença <http://www.gnu.org/licenses/>.
+ *   
+ *   
+ *   Developers:
+ *   	Marcos Vinicius Soares ( Markinster ) - e-mail: markinster@gmail.com
+ *
+ *************************************************************************************
+ */
 package com.jdbernate;
 
 import java.io.IOException;
@@ -20,7 +54,7 @@ import com.jdbernate.typemakers.JavaTypeMakerMySQL;
 import com.jdbernate.typemakers.TypeMakerHelper;
 
 public class JDbernate {
-	
+
 	// this TypeMaker is instanced in constructor of class
 	public static ITypeMaker typeMaker;
 
@@ -32,54 +66,54 @@ public class JDbernate {
 		if (DataBaseConnector.getInstance().getSgbd().equals(DataBaseConnector.SGBD_MYSQL)) {
 			typeMaker = new JavaTypeMakerMySQL();
 		}
-	}	
+	}
 
 	/*
 	 * this process receive a connection and will generate the JDBernate
 	 * architecture
 	 */
 	public void execute() throws SQLException, IOException {
-		
+
 		if (typeMaker == null)
 			typeMaker = TypeMakerHelper.getTypeMaker();
-				
+
 		tables = DBProviderBuilder.getDBProvider().getTables();
 
 		IFileWriter classFW = null;
-		IFileWriter daoFW = null;	
+		IFileWriter daoFW = null;
 		IFileWriter utilFW = null;
-		
-		//idefine program language
+
+		// idefine program language
 		if (DataBaseConnector.getInstance().isCSharp()) {
 			classFW = new CSharpClassFW();
-			daoFW = new CSharpDaoFW();	
+			daoFW = new CSharpDaoFW();
 			utilFW = new CSUtilsFW();
 		} else {
 			classFW = new JavaClassFW();
 			daoFW = new JavaDaoFW();
 		}
-		
+
 		ClassMaker classMaker = new ClassMaker();
-		
+
 		System.out.println("\n[INFO] Processing \n");
-		
+
 		for (String table : tables) {
 			ClassScheme clazz = classMaker.builder(table);
 
-			//create utils
+			// create utils
 			if (utilFW != null)
 				utilFW.write(clazz);
 
-			//create entity
-			classFW.write(clazz);		
-			
-			System.out.print(".");	
-			
-			//create DAO
+			// create entity
+			classFW.write(clazz);
+
+			System.out.print(".");
+
+			// create DAO
 			daoFW.write(clazz);
-			
+
 		}
-		
+
 		System.out.print(" \\o/");
 		System.out.println("\n\n[INFO] Process finished ");
 	}
